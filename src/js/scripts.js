608841119089a0d43be620363f3518dfb931f968
Xavier SENENTE <xavier.senente@univ-fcomte.fr>
2024-11-21 08:48:06 +0100

    21 nov

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,8 @@
+import Flickity from "flickity";
+import Headroom from "headroom.js";
+
+// AFFICHAGE DU MENU POUR LE MOBILE
+
 const hamburger = document.querySelector(".menuBurger");
 const menu = document.querySelector(".menu");
 const page = document.body;
@@ -13,4 +18,44 @@ if (hamburger && menu) {
     menu.classList.toggle("menu--open", isClosed);
     page.classList.toggle("noscroll", isClosed);
   });
+}
+
+// AFFICHAGE DU FORMULAIRE DE RECHERCHE
+const openCtrl = document.querySelector(".header__search");
+const closeCtrl = document.querySelector(".searchForm__close");
+const searchContainer = document.querySelector(".searchForm");
+const inputSearch = document.querySelector(".searchForm__input");
+
+function openSearch() {
+  searchContainer.classList.add("searchForm--open");
+  inputSearch.focus();
+}
+
+function closeSearch() {
+  searchContainer.classList.remove("searchForm--open");
+  inputSearch.value = "";
+}
+
+openCtrl.addEventListener("click", openSearch);
+closeCtrl.addEventListener("click", closeSearch);
+
+// PARAMETRAGE CAROUSEL FLICKITY
+
+const carousel = document.querySelector(".carousel");
+if (carousel) {
+  const flkty = new Flickity(carousel, {
+    wrapAround: true,
+    imagesLoaded: true,
+    lazyLoad: 3,
+    cellAlign: "left",
+  });
+}
+
+// PARAMETRAGE POUR LE HEADER AVEC HEADROOM
+const navBar = document.querySelector(".headroom");
+if (navBar) {
+  const headroom = new Headroom(navBar, {
+    offset: 200,
+  });
+  headroom.init();
 }
